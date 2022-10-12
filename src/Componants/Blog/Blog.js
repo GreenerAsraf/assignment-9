6fae81063d93b61bdfd91c5c0c5e8a09a1ccef6a
Asraf407 <Asraf.uddin4407@gmail.com>
2022-10-12 15:26:39 +0600

    blog added

--- src/Componants/Blog/Blog.js
+++ src/Componants/Blog/Blog.js
@@ -3,7 +3,36 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h3>This is a blog</h3>
+           <h3 className='text-center'>Welcome  to our Tech Blog</h3>
+
+           <div>
+           <h4>Question</h4>
+           <p>What is the purpose of React Router?</p>
+           <hr />
+           <h4>Answer</h4>
+           <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae expedita vero illum suscipit. Libero, perspiciatis magnam at vero, impedit soluta, ea ad quaerat quam molestias id! Aperiam molestiae quae aliquid.
+           </p>
+           </div>
+           <div>
+           <h4>Question</h4>
+           <p>How does Context API work?</p>
+           <hr />
+           <h4>Answer</h4>
+           <p>
+            
+           </p>
+           </div>
+           <div>
+           <h4>Question</h4>
+           <p>Describe UseRef Hook</p>
+           <hr />
+           <h4>Answer</h4>
+           <p>
+            
+           </p>
+           </div>
+
         </div>
     );
 };
